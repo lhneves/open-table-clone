6b5d080ad00fa0ae2d32f5fe9bdf035b42a2de67
Luiz Doretto <luiz.n.doretto@gmail.com>
2023-11-03 11:50:14 -0300

    feat: create header component with a search bar and add it to index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import Header from './components/Header';
+
 export default function Home() {
-  return <main className="text-center">Hello World!</main>;
+  return (
+    <main>
+      <Header />
+      <div className="py-3 px-36 mt-10 flex flex-wrap justify-start">
+        {/* Restaurants Will Be Placed Here */}
+      </div>
+    </main>
+  );
 }
