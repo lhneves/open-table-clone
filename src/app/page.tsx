2aa433a3beed48d238e9bf13215ee2296eae66a2
Luiz Doretto <luiz.n.doretto@gmail.com>
2023-11-03 12:11:51 -0300

    feat: fetch restaurants with prisma and adapt restaurant card to show restaurant info
    Still need to show price and stars on a proper component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,47 @@
 import Header from './components/Header';
-import RestaurantCard from './components/RestaurantCard';
 
-export default function Home() {
+import prisma from '../../prisma/db';
+import { Cuisine, Location, PRICE, Review } from '@prisma/client';
+
+import RestaurantCard from './components/RestaurantCard';
+export interface RestaurantCardType {
+  id: number;
+  name: string;
+  slug: string;
+  main_image: string;
+  cuisine: Cuisine;
+  location: Location;
+  price: PRICE;
+  reviews: Review[];
+}
+
+const fetchRestaurants = async (): Promise<RestaurantCardType[]> => {
+  const restaurants = await prisma.restaurant.findMany({
+    select: {
+      id: true,
+      name: true,
+      slug: true,
+      main_image: true,
+      cuisine: true,
+      location: true,
+      price: true,
+      reviews: true,
+    },
+  });
+
+  return restaurants;
+};
+
+export default async function Home() {
+  const restaurants = await fetchRestaurants();
+
   return (
     <main>
       <Header />
       <div className="py-3 px-36 mt-10 flex flex-wrap justify-start">
-        <RestaurantCard />
+        {restaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+        ))}
       </div>
     </main>
   );
