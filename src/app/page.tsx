dd459fc1290dcefd65bcb3a8bea5f1ab6e361503
Luiz Doretto <luiz.n.doretto@gmail.com>
2023-11-03 11:57:51 -0300

    feat: create 1 mocked restaurant card and add it to index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Header from './components/Header';
+import RestaurantCard from './components/RestaurantCard';
 
 export default function Home() {
   return (
     <main>
       <Header />
       <div className="py-3 px-36 mt-10 flex flex-wrap justify-start">
-        {/* Restaurants Will Be Placed Here */}
+        <RestaurantCard />
       </div>
     </main>
   );
