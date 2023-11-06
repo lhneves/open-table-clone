75c6a239777c03f9b5a2b454341fdf8197505a2f
Luiz Doretto <luiz.n.doretto@gmail.com>
2023-11-06 09:26:55 -0300

    feat: create navbar component to authenticate users and add it on layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Navbar from './components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,7 +13,15 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="bg-gray-100 min-h-screen w-screen">
+          <main className="max-w-screen-2xl m-auto bg-white">
+            <Navbar />
+
+            <main>{children}</main>
+          </main>
+        </main>
+      </body>
     </html>
   );
 }
