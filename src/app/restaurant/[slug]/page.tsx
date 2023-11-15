2000d18088176e32b6fb6a60e44dc47c0db1a300
Luiz Doretto <luiz.n.doretto@gmail.com>
2023-11-15 18:33:35 -0300

    feat: apply new components to screen, fetching restaurant by slug

--- src/app/restaurant/[slug]/page.tsx
+++ src/app/restaurant/[slug]/page.tsx
@@ -1,96 +1,67 @@
 import React from 'react';
 import RestaurantNavbar from '../components/RestaurantNavbar';
 import Title from '../components/Title';
+import Rating from '../components/Rating';
+import prisma from '../../../../prisma/db';
+import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
+import Description from '../components/Description';
+import Images from '../components/Images';
+import Reviews from '../components/Reviews';
+
+type Props = {
+  params: { slug: string };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const renderTitle = () => {
+    const nameArray = params.slug.split('-');
+
+    nameArray[nameArray.length - 1] = `(${nameArray[nameArray.length - 1]})`;
+
+    return nameArray.join(' ');
+  };
+
+  return {
+    title: `${renderTitle()} | OpenTable`,
+    description: `Reserve a table in ${renderTitle()}`,
+  };
+}
+
+const fetchRestaurantBySlug = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: { slug },
+    select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true,
+      reviews: true,
+      open_time: true,
+      close_time: true,
+    },
+  });
+
+  if (!restaurant) {
+    notFound();
+  }
+
+  return restaurant;
+};
 
 export default async function RestaurantDetails({ params }: { params: { slug: string } }) {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
+
   return (
     <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
         <RestaurantNavbar slug={params.slug} />
         <Title name="Milesstone Grill" />
-        {/* RATING */}
-        <div className="flex items-end">
-          <div className="ratings mt-2 flex items-center">
-            <p>*****</p>
-            <p className="text-reg ml-3">4.9</p>
-          </div>
-          <div>
-            <p className="text-reg ml-4">600 Reviews</p>
-          </div>
-        </div>
-        {/* RATING */} {/* DESCRIPTION */}
-        <div className="mt-4">
-          <p className="text-lg font-light">
-            The classics you love prepared with a perfect twist, all served up in an atmosphere that
-            feels just right. That’s the Milestones promise. So, whether you’re celebrating a
-            milestone, making the most of Happy Hour or enjoying brunch with friends, you can be
-            sure that every Milestones experience is a simple and perfectly memorable one.
-          </p>
-        </div>
-        {/* DESCRIPTION */} {/* IMAGES */}
-        <div>
-          <h1 className="font-bold text-3xl mt-10 mb-7 border-b pb-5">5 photos</h1>
-          <div className="flex flex-wrap">
-            <img
-              className="w-56 h-44 mr-1 mb-1"
-              src="https://resizer.otstatic.com/v2/photos/xlarge/3/41701449.jpg"
-              alt=""
-            />
-            <img
-              className="w-56 h-44 mr-1 mb-1"
-              src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701450.jpg"
-              alt=""
-            />
-            <img
-              className="w-56 h-44 mr-1 mb-1"
-              src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701452.jpg"
-              alt=""
-            />
-            <img
-              className="w-56 h-44 mr-1 mb-1"
-              src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701453.jpg"
-              alt=""
-            />
-            <img
-              className="w-56 h-44 mr-1 mb-1"
-              src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701454.jpg"
-              alt=""
-            />
-          </div>
-        </div>
-        {/* IMAGES */} {/* REVIEWS */}
-        <div>
-          <h1 className="font-bold text-3xl mt-10 mb-7 borber-b pb-5">
-            What 100 people are saying
-          </h1>
-          <div>
-            {/* REVIEW CARD */}
-            <div className="border-b pb-7 mb-7">
-              <div className="flex">
-                <div className="w-1/6 flex flex-col items-center">
-                  <div className="rounded-full bg-blue-400 w-16 h-16 flex items-center justify-center">
-                    <h2 className="text-white text-2xl">MJ</h2>
-                  </div>
-                  <p className="text-center">Micheal Jordan</p>
-                </div>
-                <div className="ml-10 w-5/6">
-                  <div className="flex items-center">
-                    <div className="flex mr-5">*****</div>
-                  </div>
-                  <div className="mt-5">
-                    <p className="text-lg font-light">
-                      Laurie was on top of everything! Slow night due to the snow storm so it worked
-                      in our favor to have more one on one with the staff. Delicious and well worth
-                      the money.
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* REVIEW CARD */}
-          </div>
-        </div>
-        {/* REVIEWS */}
+        <Rating reviews={restaurant.reviews} />
+        <Description description={restaurant.description} />
+        <Images images={restaurant.images} />
+        <Reviews reviews={restaurant.reviews} />
       </div>
       <div className="w-[27%] relative text-reg">
         <div className="fixed w-[15%] bg-white rounded p-3 shadow">
