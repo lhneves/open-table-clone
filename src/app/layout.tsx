41fa2b6562b653079a193d905d6fa892755b786f
Luiz Doretto <luiz.n.doretto@gmail.com>
2023-11-20 09:58:50 -0300

    feat: create context and apply it to application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Navbar from './components/Navbar';
+import AuthContext from './context/AuthContext';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,11 +16,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={inter.className}>
         <main className="bg-gray-100 min-h-screen w-screen">
-          <main className="max-w-screen-2xl m-auto bg-white">
-            <Navbar />
+          <AuthContext>
+            <main className="max-w-screen-2xl m-auto bg-white">
+              <Navbar />
 
-            <main>{children}</main>
-          </main>
+              <main>{children}</main>
+            </main>
+          </AuthContext>
         </main>
       </body>
     </html>
