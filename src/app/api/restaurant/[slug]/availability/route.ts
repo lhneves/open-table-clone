88be9c53bcb703fa025813a9deced9e942508b61
Luiz Doretto <luiz.n.doretto@gmail.com>
2023-11-21 15:32:50 -0300

    feat: create availability api route and service

---
+++ src/app/api/restaurant/[slug]/availability/route.ts
@@ -0,0 +1,63 @@
+import { findAvailableTables } from '@/services/restaurant/findAvailableTables';
+import { PrismaClient } from '@prisma/client';
+import { NextRequest, NextResponse } from 'next/server';
+
+const prisma = new PrismaClient();
+
+export async function GET(req: NextRequest, { params }: { params: { slug: string } }) {
+  const slug = params.slug;
+
+  const searchParams = req.nextUrl.searchParams;
+
+  const day = searchParams.get('day');
+  const time = searchParams.get('time');
+  const partySize = searchParams.get('partySize');
+
+  if (!day || !time || !partySize) {
+    return NextResponse.json({ errorMessage: 'Invalid data provided' }, { status: 400 });
+  }
+
+  const restaurant = await prisma.restaurant.findUnique({
+    where: { slug },
+    select: {
+      tables: true,
+      open_time: true,
+      close_time: true,
+    },
+  });
+
+  if (!restaurant) {
+    return NextResponse.json({ errorMessage: 'Invalid data provided' }, { status: 400 });
+  }
+
+  const searchTimesWithTables = await findAvailableTables({
+    time,
+    day,
+    restaurant,
+  });
+
+  if (!searchTimesWithTables || !Array.isArray(searchTimesWithTables)) {
+    return NextResponse.json({ errorMessage: 'Invalid data provided' }, { status: 400 });
+  }
+
+  const availabilities = searchTimesWithTables
+    .map((t) => {
+      const sumSeats = t.tables.reduce((sum, table) => {
+        return sum + table.seats;
+      }, 0);
+      console.log(sumSeats, partySize);
+      return { time: t.time, available: sumSeats >= parseInt(partySize) };
+    })
+    .filter((availability) => {
+      const timeIsAfterOpeningHours =
+        new Date(`${day}T${availability.time}`) >= new Date(`${day}T${restaurant.open_time}`);
+
+      const timeIsBeforeClosingHours =
+        new Date(`${day}T${availability.time}`) <= new Date(`${day}T${restaurant.close_time}`);
+
+      return timeIsAfterOpeningHours && timeIsBeforeClosingHours;
+    });
+
+  console.log(availabilities);
+  return NextResponse.json(availabilities);
+}
